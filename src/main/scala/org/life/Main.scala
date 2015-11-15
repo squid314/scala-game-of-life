0a0e8888c22289777b5f59fa47ae8dbd405672aa
squid314 <squid314@users.noreply.github.com>
2015-11-15 14:44:30 -0700

    move files to a non-root package

---
+++ src/main/scala/org/life/Main.scala
@@ -0,0 +1,16 @@
+package org.life
+
+object Main {
+    def main( args: Array[ String ] ) = {
+        println( "Hello, world!" )
+    }
+}
+
+object ConwaysGameOfLifeRunner extends App {
+    println( "<game of life will run here>" )
+}
+
+/** Represents a Game of Life board which is bounded by a destructive border. */
+class BoundedBoard {
+    
+}
