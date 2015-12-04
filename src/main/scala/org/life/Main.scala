4d1c598b99939f905baebb1a35c20670d591a437
squid314 <squid314@users.noreply.github.com>
2015-12-04 00:08:44 -0700

    refactor and try out implicit class

--- src/main/scala/org/life/Main.scala
+++ src/main/scala/org/life/Main.scala
@@ -10,9 +10,26 @@ object ConwaysGameOfLifeRunner extends App {
     def allPositions( height: Int, width: Int ): Positions = {
         for {x <- 0 until height; y <- 0 until width} yield (x, y)
     }.toSet
+
     def randomPositions( height: Int, width: Int, populationRatio: Double ): Positions =
         allPositions( height, width )
                 .filter( e => Math.random < populationRatio )
+
+    implicit class BoardStream[ B <: AbstractMatrixBackedBoard[ B ] ]( stream: Stream[ (B, Int) ] ) {
+        def haltAfterSteadyState = {
+            def noStateMatchesPrevious( items: Stream[ (B, Int) ] ): Boolean = {
+                val (current, _) = items.last
+                !( items.init exists { case (prev, _) => current == prev } )
+            }
+            // will catch boards which are steady state and boards which oscilate (blinkers) at period 2
+            val maxFoundPeriod = 2
+            // TODO rewrite this to not require the stream to have initial throwaway elements
+            stream
+                    .sliding( maxFoundPeriod + 1 )
+                    .takeWhile( noStateMatchesPrevious )
+                    .map( _.last ) // get back to the board we care about
+        }
+    }
 
     val (height, width) = (50, 80)
     val populationRatio = 0.3333
@@ -20,25 +37,17 @@ object ConwaysGameOfLifeRunner extends App {
         offsetBy( 10, 10 )( blinker )
 //        randomPositions( height, width, populationRatio )
 
-    val initBoard = MatrixBoardFactory.toroid( height, width )( initialPositions )
+    val initBoard = MatrixBoardFactory.bounded( height, width )( initialPositions )
     // this type cannot be specified explicitly lest we require updating it each time we change from toroid to bounded
-    val boardStream = Stream.iterate( initBoard )( _.nextBoard( ) )
+    val boardStream = Stream( (null, -2), (null, -1) ) ++
+            Stream.iterate( initBoard )( _.nextBoard( ) ).zipWithIndex
 
     val (short, medium, long, epic) = (20, 150, 1125, 8437)
     val (numbing, slow, peppy, fast) = (500, 250, 125, 62)
     val (duration, delay) = (medium, fast)
     boardStream
-            .sliding( 3 )
-            .takeWhile( items => {
-                val prevPrev = items.head
-                val prev = items.tail.head
-                val current = items.tail.tail.head
-                // will catch boards which are steady state and boards which oscilate (blinkers)
-                current != prev && current != prevPrev
-            } )
+            .haltAfterSteadyState
             .take( duration )
-            .map( _.tail.tail.head ) // get back to the board we care about (note that we lose printout of the initial board and the first generation; whatever)
-            .zipWithIndex
             .foreach { case (t, index) =>
                 println
                 println( t )
